19e0c58e374bfdaee9fd490d1287d9d1f3309877
williamvlelasquez2904 <wavm290472@gmail.com>
2025-06-18 12:11:55 -0400

    upd defautl

--- default.php
+++ default.php
@@ -1,8 +1,10 @@
 <a href="https://www.flaticon.es/iconos-gratis/proceso" title="Registrar Nota" target="_blank" style="display:inline-block; margin-left: 40px;">
     <img src="img/notas.png" alt="Registrar" width="52" height="52" style="vertical-align:middle;" />
+    <span style="font-size:18px; vertical-align:middle; margin-left:12px;">Registrar nota</span>
 </a>
 <br>
 <a href="https://www.flaticon.es/iconos-gratis/reporte" title="Reporte de Matriz por Proveedor" target="_blank" style="display:inline-block; margin-left: 40px; margin-top: 30px;">
     <img src="img/reporte1.png" alt="Reporte de Matriz por Proveedor" width="52" height="52" style="vertical-align:middle;" />
+    <span style="font-size:18px; vertical-align:middle; margin-left:12px;">Reporte Matriz</span>
 </a>
 
