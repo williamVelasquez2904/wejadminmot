9372069def8f76d78962de48b4e4d0f50ba9e16a
williamvlelasquez2904 <wavm290472@gmail.com>
2025-07-08 22:30:20 -0400

    iconos en index

--- default.php
+++ default.php
@@ -1,11 +1,13 @@
 <!-- Agrega un id al div principal para poder ocultarlo -->
 <div id="default-content">
-    <a href="app/compra_wh/vst/admin.php" title="Registrar Nota" style="display:inline-block; margin-left: 40px;" target="main"
+    <a href="app/compra_wh/vst/admin.php" title="Registrar Nota"
+       style="display:inline-block; margin-left: 40px; text-decoration: none;" target="main"
        onclick="document.getElementById('default-content').style.display='none';load('vst-compra_wh-admin','origen_opcion='+1,'.registrar_compra'); return false;">
         <img src="img/notas.png" alt="Registrar" width="52" height="52" style="vertical-align:middle;" />
         <span style="font-size:18px; vertical-align:middle; margin-left:12px; color: #ff6600;">Registrar nota</span>
     </a>
-    <a href="#" title="Reporte de Matriz por Proveedor" style="display:inline-block; margin-left: 60px;"
+    <a href="#" title="Reporte de Matriz por Proveedor"
+       style="display:inline-block; margin-left: 60px; text-decoration: none;"
        onclick="document.getElementById('default-content').style.display='none'; load('vst-compra_wh-lista_matriz','','.lista_matriz'); return false;">
         <img src="img/reporte1.png" alt="Matriz por Proveedor" width="52" height="52" style="vertical-align:middle;" />
         <span style="font-size:18px; vertical-align:middle; margin-left:12px; color: #ff6600;">Matriz por Proveedor</span>
@@ -20,6 +22,7 @@
     transform: translateY(-50%);
     z-index: 1000;
     display: inline-block;
+    text-decoration: none;
 "
    onclick="document.getElementById('default-content').style.display='none'; document.getElementById('icono-cobranza').style.display='none'; load('vst-reportes-admin_cobranza','origen_opcion='+1,'.cobranza'); return false;">
     <img src="img/icono-cobranza.png" alt="Cobranza" width="60" height="60" style="vertical-align:middle;" />
